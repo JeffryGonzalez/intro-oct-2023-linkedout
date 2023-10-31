928e470024f065a8479c467ff82eb25308acaed3
Jeffry Gonzalez <jeff@hypertheory.com>
2023-10-31 15:15:01 -0400

    count by done

--- frontend/frontend/src/app/pages/components/count-by.component.ts
+++ frontend/frontend/src/app/pages/components/count-by.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Store } from "@ngrx/store";
-import { CountByValues } from "src/app/state/counter";
+import { CountByValues, CounterFeature } from "src/app/state/counter";
 import { CounterCommands } from "src/app/state/counter.actions";
 
 @Component({
@@ -10,16 +10,34 @@ import { CounterCommands } from "src/app/state/counter.actions";
   imports: [CommonModule],
   template: `
     <div class="join">
-      <button (click)="setCountBy(1)" class="btn join-item">Count By 1</button>
-      <button (click)="setCountBy(3)" class="btn join-item">Count By 3</button>
-      <button (click)="setCountBy(5)" class="btn join-item">Count By 5</button>
+      <button
+        [disabled]="countingBy() === 1"
+        (click)="setCountBy(1)"
+        class="btn join-item"
+      >
+        Count By 1
+      </button>
+      <button
+        [disabled]="countingBy() === 3"
+        (click)="setCountBy(3)"
+        class="btn join-item"
+      >
+        Count By 3
+      </button>
+      <button
+        [disabled]="countingBy() === 5"
+        (click)="setCountBy(5)"
+        class="btn join-item"
+      >
+        Count By 5
+      </button>
     </div>
   `,
   styles: [],
 })
 export class CountByComponent {
   store = inject(Store);
-
+  countingBy = this.store.selectSignal(CounterFeature.selectBy);
   setCountBy(by: CountByValues) {
     this.store.dispatch(CounterCommands.setCountBy({ by }));
   }
