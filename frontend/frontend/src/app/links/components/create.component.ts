a1e5e1aa9630e07b482a7a7d8e9af8746d69a9a1
Jeffry Gonzalez <jeff@hypertheory.com>
2023-11-02 11:17:23 -0400

    links started

---
+++ frontend/frontend/src/app/links/components/create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      create works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CreateComponent {
+
+}
